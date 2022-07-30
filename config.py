d179c1db92df06ec3db6c6a02a4596ff60da5210
ConfirmedPlayer <confirmedplayer@mail.ru>
2022-07-30 20:35:52 +0500

    removed message_send url

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ api_link = 'https://temnomor.ru/api/groups?group={group}&sid={sid}&gr={gr}'
 
 group_id = 212422399
 admin_user_id = 641064938 # Admin commands will work only for ID provided
-message_send_url = 'https://api.vk.com/method/messages.send?access_token={token}&v=5.130&peer_id={peer_id}&attachment={attachment}&random_id={random_id}'
 
 
 schedule_menu_url = 'http://mnokol.tyuiu.ru/rtsp/index2.php'
