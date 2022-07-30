571c4991a25be5ca92eb8bddf80f4b4c6d1c169a
ConfirmedPlayer <confirmedplayer@mail.ru>
2022-07-30 20:35:39 +0500

    removed logs from middleware

--- blueprints/middlewares.py
+++ blueprints/middlewares.py
@@ -1,6 +1,5 @@
 from vkbottle import BaseMiddleware
 from vkbottle.bot import Blueprint, Message
-from loguru import logger
 
 
 bp = Blueprint('Middlewares')
@@ -9,9 +8,6 @@ bp = Blueprint('Middlewares')
 class NoBotMiddleware(BaseMiddleware[Message]):
     async def pre(self):
         if self.event.from_id < 0:
-            logger.warning(f"{self.event.from_id} / {self.event.peer_id} tried to use bot. \
-                More details:\n\n{self.event}")
-
             self.stop()
 
 
