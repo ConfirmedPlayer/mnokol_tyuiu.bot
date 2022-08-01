98d8049c8efc347bbdc4edb95054cf487b588ea3
ConfirmedPlayer <confirmedplayer@mail.ru>
2022-08-01 16:34:42 +0500

    added admin_user_id validation

--- config.py
+++ config.py
@@ -8,10 +8,13 @@ api_link = 'https://temnomor.ru/api/groups?group={group}&sid={sid}&gr={gr}'
 
 
 group_id = 212422399
-admin_user_id = 641064938 # Admin commands will work only for ID provided
+admin_user_id = 641064938  # Admin commands will work only for ID provided
 
 
 schedule_menu_url = 'http://mnokol.tyuiu.ru/rtsp/index2.php'
 
 
 chrome_path = '/usr/bin/google-chrome'
+
+
+assert group_id > 0
